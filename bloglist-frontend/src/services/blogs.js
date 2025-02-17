33a602188d43dafa8aaefb285c0acee855b544d9
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-02-17 15:39:07 +0200

    Part 5 Exercise 10 - Blogs are now sorted by number of likes

--- bloglist-frontend/src/services/blogs.js
+++ bloglist-frontend/src/services/blogs.js
@@ -7,9 +7,11 @@ const setToken = newToken => {
   token = `Bearer ${newToken}`
 }
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  const blogs = response.data
+  blogs.sort((a, b) => b.likes - a.likes)
+  return blogs
 }
 
 const create = async newBlog => {

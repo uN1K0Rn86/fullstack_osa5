ee573600f3123e08cf5ee7aec19e715a8df9bb93
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-02-26 10:26:52 +0200

    Part 5 Exercise 14 - Added test for displaying more information

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('Blog title and author are rendered', async () => {
@@ -27,6 +28,44 @@ test('Blog title and author are rendered', async () => {
 
     const title = await screen.findAllByText('Mental Health for Heralds')
     const author = await screen.findAllByText('Kaladin Stormblessed')
+
     expect(title).toBeDefined()
     expect(author).toBeDefined()
+})
+
+test('Blog url, likes, and username are rendered after show more -button is clicked', async () => {
+    const blog = {
+        title: 'Choosing the Right Clothes with the Right Sword',
+        author: 'Adolin Kholin',
+        url: 'https://roshar.com/ad0lin',
+        likes: 1919191919,
+        user: {
+            id: '80085',
+            name: 'Shallan Davar',
+            username: 'Sh4ll4n'
+        }
+    }
+
+    const mockLike = vi.fn()
+    const mockDelete = vi.fn()
+    const username = 'Sh4ll4n'
+
+    render(<Blog
+        blog={blog}
+        like={mockLike}
+        username={username}
+        remove={mockDelete}
+    />)
+
+    const user = userEvent.setup()
+    const showButton = screen.getByText('Show More')
+    await user.click(showButton)
+
+    const url = await screen.findByText(/https:\/\/roshar\.com\/ad0lin/)
+    const likes = await screen.findByText(/1919191919/)
+    const showUser = await screen.findByText(/Sh4ll4n/)
+
+    expect(url).toBeDefined()
+    expect(likes).toBeDefined()
+    expect(showUser).toBeDefined()
 })

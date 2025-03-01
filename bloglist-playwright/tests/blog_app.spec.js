857e5cc056f82194ad2633d214bbe2b4d0184867
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-03-01 11:49:36 +0200

    Part 5 Exercise 23 - Added test making sure blogs are in order of likes

--- bloglist-playwright/tests/blog_app.spec.js
+++ bloglist-playwright/tests/blog_app.spec.js
@@ -1,5 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-const { loginWith, addNewBlog, openBlogInfo, logout } = require('./helper')
+const { loginWith, addNewBlog, openBlogInfo, logout, like } = require('./helper')
 
 describe('Blog app', () => {
     beforeEach(async ({ page, request }) => {
@@ -30,13 +30,13 @@ describe('Blog app', () => {
 
     describe('Login', () => {
         test('succeeds with correct credentials', async ({ page }) => {
-            loginWith(page, 'QuickBen', 'Kalam4eva')
+            await loginWith(page, 'QuickBen', 'Kalam4eva')
 
             await expect(page.getByText('Ben Adaephon Delat logged in')).toBeVisible()
         })
 
         test('fails with incorrect credentials', async ({ page }) => {
-            loginWith(page, 'QuickBen', 'Kalamnot4eva')
+            await loginWith(page, 'QuickBen', 'Kalamnot4eva')
 
             await expect(page.getByText('Incorrect username or password')).toBeVisible()
         })
@@ -44,8 +44,8 @@ describe('Blog app', () => {
 
     describe('When logged in', () => {
         beforeEach(async ({ page }) => {
-            loginWith(page, 'QuickBen', 'Kalam4eva')
-            addNewBlog(
+            await loginWith(page, 'QuickBen', 'Kalam4eva')
+            await addNewBlog(
                 page,
                 'Blankets and their Many Uses',
                 'Tehol Beddict',
@@ -54,7 +54,7 @@ describe('Blog app', () => {
         })
 
         test('a new blog can be created', async ({ page }) => {
-            addNewBlog(
+            await addNewBlog(
                 page,
                 'Walking in Walls (Capustan style)',
                 'Ben Delat',
@@ -67,23 +67,20 @@ describe('Blog app', () => {
         })
 
         test('a blog can be liked', async ({ page }) => {
-            const row = await page.locator('tr', { name: 'Blankets and their Many Uses' })
-            openBlogInfo(page, row)
-
-            const likesText = await row.locator('span:has-text("Likes:")').textContent()
-            const likes = parseInt(likesText.replace('Likes: ', ''), 10)
-            await row.locator('button', { hasText: 'Like' }).click()
-            await page.waitForTimeout(500)
+            const blogName = 'Blankets and their Many Uses'
+            const cell = await page.getByRole('cell', { name: blogName })
+            const row = await cell.locator('..')
+            await like(page, blogName)
 
             const newLikesText = await row.locator('span:has-text("Likes:")').textContent()
             const newLikes = parseInt(newLikesText.replace('Likes: ', ''), 10)
 
-            await expect(newLikes - likes).toEqual(1)
+            await expect(newLikes).toEqual(1)
         })
 
         test('a blog can be deleted by the user who added it', async ({ page }) => {
             const row = await page.locator('tr', { name: 'Blankets and their Many Uses' })
-            openBlogInfo(page, row)
+            await openBlogInfo(page, row)
 
             page.on('dialog', async (dialog) => {
                 await dialog.accept()
@@ -98,16 +95,92 @@ describe('Blog app', () => {
 
         test('the remove button is not visible if the blog was added by another user', async ({ page }) => {
             const row = await page.locator('tr', { name: 'Blankets and their Many Uses' })
-            openBlogInfo(page, row)
+            await openBlogInfo(page, row)
             await expect(row.locator('button', { hasText: 'Remove' })).toBeVisible()
-            logout(page)
-            loginWith(page, 'Shadowthrone', 'D4ncer')
+            await logout(page)
+            await loginWith(page, 'Shadowthrone', 'D4ncer')
 
             await page.getByText('Kellanved logged in').waitFor()
             const newRow = await page.locator('tr', { name: 'Blankets and their Many Uses' })
-            openBlogInfo(page, newRow)
+            await openBlogInfo(page, newRow)
             await expect(newRow.locator('button', { hasText: 'Show Less' })).toBeVisible()
             await expect(newRow.locator('button', { hasText: 'Remove' })).not.toBeVisible()
+        })
+
+        test('blogs are sorted by likes, most to least', async ({ page }) => {
+            await addNewBlog(
+                page,
+                'Walking in Walls (Capustan style)',
+                'Ben Delat',
+                'https://genabackis.blogspot.com'
+            )
+            await addNewBlog(
+                page,
+                'Slaying Children',
+                'Karsa Orlong',
+                'https://teblor.org/blogs'
+            )
+            await addNewBlog(
+                page,
+                'Secrets of Thyr',
+                'Tattersail',
+                'https://secondarmycadre.org/blogs'
+            )
+            await addNewBlog(
+                page,
+                'Meanas and Rashan - Intricacies',
+                'Bottle',
+                'https://thefourteenth.com/blogs'
+            )
+            await addNewBlog(
+                page,
+                'My Many Candles',
+                'Beak',
+                'https://thefourteenth.com/blogs'
+            )
+
+            await like(page, 'My Many Candles')
+            await like(page, 'My Many Candles')
+            await like(page, 'My Many Candles')
+            await like(page, 'My Many Candles')
+            await like(page, 'My Many Candles')
+            await like(page, 'My Many Candles')
+
+            await like(page, 'Slaying Children')
+            await like(page, 'Slaying Children')
+            await like(page, 'Slaying Children')
+            await like(page, 'Slaying Children')
+            await like(page, 'Slaying Children')
+
+            await like(page, 'Meanas and Rashan - Intricacies')
+            await like(page, 'Meanas and Rashan - Intricacies')
+            await like(page, 'Meanas and Rashan - Intricacies')
+            await like(page, 'Meanas and Rashan - Intricacies')
+
+            await like(page, 'Walking in Walls (Capustan style)')
+            await like(page, 'Walking in Walls (Capustan style)')
+            await like(page, 'Walking in Walls (Capustan style)')
+            
+            await like(page, 'Secrets of Thyr')
+            await like(page, 'Secrets of Thyr')
+
+            await like(page, 'Blankets and their Many Uses')
+
+            const rowCount = await page.getByRole('row').count()
+
+            await expect(rowCount).toEqual(7)
+
+            for (let i = 1; i < rowCount - 1; i++) {
+                const row = page.getByRole('row').nth(i)
+                const likesText = await row.locator('span:has-text("Likes:")').textContent()
+                const likes = parseInt(likesText.replace('Likes: ', ''), 10)
+
+                const nextRow = page.getByRole('row').nth(i + 1)
+                const likesTextNext = await nextRow.locator('span:has-text("Likes:")').textContent()
+                const likesNext = parseInt(likesTextNext.replace('Likes: ', ''), 10)
+
+                await expect(likes).toBeGreaterThanOrEqual(likesNext)
+            }
         })
     })
 })
--- bloglist-playwright/tests/helper.js
+++ bloglist-playwright/tests/helper.js
@@ -10,17 +10,31 @@ const addNewBlog = async (page, title, author, url) => {
     await page.getByTestId('title').fill(title)
     await page.getByTestId('author').fill(author)
     await page.getByTestId('url').fill(url)
-
     await page.getByRole('button', { name: 'Create' }).click()
-    await page.getByRole('cell', { name: author }).waitFor()
+    await page.getByRole('row', { name: title }).waitFor()
 }
 
 const openBlogInfo = async (page, row) => {
     await row.locator('button', { hasText: 'Show More' }).click()
+    await row.locator('button', { hasText: 'Show Less' }).waitFor()
 }
 
 const logout = async (page) => {
     await page.getByRole('button', { name: 'Logout' }).click()
 }
 
-export { loginWith, addNewBlog, openBlogInfo, logout }
+const like = async (page, blogName) => {
+    const cell = await page.getByRole('cell', { name: blogName })
+    const row = await cell.locator('..')
+
+    if (await row.locator('button', { hasText: 'Show More' }).isVisible()) {
+        await openBlogInfo(page, row)
+    }
+    
+    const likesText = await row.locator('span:has-text("Likes:")').textContent()
+    const likes = parseInt(likesText.replace('Likes: ', ''), 10)
+    await row.locator('button', { hasText: 'Like' }).click()
+    await row.getByText(`Likes: ${likes + 1}`).waitFor()
+}
+
+export { loginWith, addNewBlog, openBlogInfo, logout, like }
